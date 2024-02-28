a7dbc29896ad14aaf339518ed5059b2b437836d6
tatxha <134766457+tatxha@users.noreply.github.com>
2024-02-28 21:45:29 +0700

    main.py

--- PYTHON/Program/main.py
+++ PYTHON/Program/main.py
@@ -1,3 +1,8 @@
+/* 
+    Saya Tattha Maharany Yasmin Akbar dengan NIM 2201805 mengerjakan soal Latihan 4 
+    dalam Praktikum mata kuliah Desain dan Pemrograman Berbasis Objek, untuk keberkahan-Nya
+    maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamin.
+*/
 
 from Vehicle import Vehicle
 from Car import Car
