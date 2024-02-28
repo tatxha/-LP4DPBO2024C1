fc2f17bde52e5695dd023c9cdfc501eb86dcb998
tatxha <134766457+tatxha@users.noreply.github.com>
2024-02-28 21:45:07 +0700

    Vehicle.py

--- PYTHON/Program/Vehicle.py
+++ PYTHON/Program/Vehicle.py
@@ -1,3 +1,9 @@
+/* 
+    Saya Tattha Maharany Yasmin Akbar dengan NIM 2201805 mengerjakan soal Latihan 4 
+    dalam Praktikum mata kuliah Desain dan Pemrograman Berbasis Objek, untuk keberkahan-Nya
+    maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamin.
+*/
+
 class Vehicle():
     __plat_nomor = ""
     __merk = ""
